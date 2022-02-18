0ecb6f32cc8c3673dcf9a35a3d67542221a12175
amitrohan <amit@kruzr.co>
2022-02-18 22:42:30 +0530

    Updates

---
+++ precache-manifest.a3ed665501d4e16c57c22b26c088fb8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649b2a3d54d89393003aea4dea144d7a",
+    "url": "/believethrift/index.html"
+  },
+  {
+    "revision": "d4f103d10049db63a61f",
+    "url": "/believethrift/static/js/2.1d496b0b.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/believethrift/static/js/2.1d496b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccb562e6c50522006bcc",
+    "url": "/believethrift/static/js/main.b8d84f11.chunk.js"
+  },
+  {
+    "revision": "fdfa0aab34112c675bd2",
+    "url": "/believethrift/static/js/runtime-main.f291a1f2.js"
+  },
+  {
+    "revision": "8374832e0f72bca461a7bd7b0f8d9ff1",
+    "url": "/believethrift/static/media/logo.8374832e.jpeg"
+  }
+]);
